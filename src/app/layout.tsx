1b3cf5441ad7a99d6e877182a430e31651429abf
djefx <dheeraj.sampath@equifax.com>
2024-09-26 14:34:44 -0500

    typo fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./Header";
+import Header from "./header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
